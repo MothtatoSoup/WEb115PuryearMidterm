694c1fbee09f07229667da4a2f70ca44239d210a
MothtatoSoup <maddy.puryear@outlook.com>
2025-04-04 13:45:51 -0400

    eeeeeee

--- amortizationCalculator.js
+++ amortizationCalculator.js
@@ -17,20 +17,19 @@ const monthIntrest = annIntrest / 12
 subButton.addEventListener("click", Check)
 
 function Check(){
-    if(loanTerm.value != 15 || 30){
-        window.alert("Invalid Loan Term")
-        console.log("Invalid Loan Term")
-    }else if(loanTerm == 15 || 30){
-        Calculate
+    if(loanTerm == 15 || 30){
         console.log("Calculating...")
+        Calculate()
+        
     }else{
-        window.alert("Error!!")
+        window.alert("Invalid loan")
         console.log("Error")
     }
 }
 
 function Calculate(){
     let month = 1
+    let totalMonths = loanTerm * 12
     let monthPay = ((monthIntrest * princLoanAmnt) / (1 - Math.pow(1 + monthIntrest, -totalMonths))).toFixed(2)
     let totalIntPaid = (monthPay * totalMonths) - princLoanAmnt
     let loanCost = princLoanAmnt + totalIntPaid
@@ -39,10 +38,10 @@ function Calculate(){
     let remLoan = loan
     moneyInfo.innerHTML = "Loan Term: " + loanTerm + " Years <br/>"
     console.log("now calculating")
-    while(remloan >= 0){
-         payPlan.innerHTML = "Monthly Pay: $" + monthPay + "Intrest pay: $" + IntPaid + "Principal Pay: $" + principalPaid + "Remaining Loan Balance : $" + remLoan + "<br/>"
+    while(remLoan >= 0){
+         payPlan.innerHTML = "Monthly Pay: $" + monthPay + " Intrest pay: $" + IntPaid + " Principal Pay: $" + principalPaid + " Remaining Loan Balance : $" + remLoan + "<br/>"
          remLoan = remLoan - monthPay
-         IntPaid = remloan * monthIntrest
+         IntPaid = remLoan * monthIntrest
          principalPaid = monthPay - IntPaid
         
     end.innerHTML = "This ends the Amortization Calculator"
